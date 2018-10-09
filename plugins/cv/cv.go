ae6a342fbc6fe57ce09ed7b1b9ff33d6388c0117
CanadianCommander <ben.benetti@shaw.ca>
2018-10-08 21:59:04 -0700

    Work + finish up basic section fetching plugin (cv.go). This plugin allows me to break sections of the page out in to sub   files, for increased maintainability. + add "testSection.gohtml" and "testSection2.gothml" for section testing / development. ps. one of them has a part of my European   Integration essay in it :p + add new media folder + assets + work on page in general, trying to make a master piece. I think it's actually not looking half bad!

--- plugins/cv/cv.go
+++ plugins/cv/cv.go
@@ -2,26 +2,41 @@ package main
 
 import (
 	"net/http"
+	"path"
+	"strings"
 
 	"github.com/CanadianCommander/MicroWeb/pkg/logger"
-	"github.com/CanadianCommander/MicroWeb/pkg/pluginUtil"
+	mwsettings "github.com/CanadianCommander/MicroWeb/pkg/mwSettings"
+	pu "github.com/CanadianCommander/MicroWeb/pkg/pluginUtil"
 )
-
-type cvParseStruct struct {
-}
-
-func (cv *cvParseStruct) GetSectionContent(name string) string {
-	return "foobar"
-}
 
 func HandleRequest(req *http.Request, res http.ResponseWriter, fsName string) bool {
 	cvParse := cvParseStruct{}
 
-	err := pluginUtil.ProcessTemplate(pluginUtil.ReadFileToBuff(fsName), res, &cvParse)
+	err := pu.ProcessTemplateText(pu.ReadFileToBuff(fsName), res, &cvParse)
 	if err != nil {
 		logger.LogError("Error Processing template: %s", err.Error())
 		return false
 	}
 
 	return true
+}
+
+type cvParseStruct struct {
+}
+
+func (cv *cvParseStruct) GetSectionContent(name string) string {
+	name = strings.Trim(name, " ")
+	buff := pu.ReadFileToBuff(ResolveSectionName(name))
+	if buff == nil {
+		logger.LogError("Could not load section file with name: %s", name)
+		return "500 internal error"
+	}
+
+	return string((*buff)[:])
+}
+
+func ResolveSectionName(name string) string {
+	staticDir := mwsettings.GlobalSettings.GetStaticResourcePath()
+	return path.Join(staticDir, "sections", name)
 }
