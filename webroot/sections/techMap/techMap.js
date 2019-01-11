8d99b027f29a64d6f35dff83615e505183ed4e90
CanadianCommander <ben.benetti@shaw.ca>
2019-01-10 22:45:40 -0800

    Writing. + I often say, I can talk to a computer quicker than I can talk to a human. This appears true. Writing takes forever. + adjust techMap scaling effect to also adjust the flex-grow of the columns to prevent spaz-tastic reorganization of items if   a row grows larger than the container when an item is being hovered or selected.

--- webroot/sections/techMap/techMap.js
+++ webroot/sections/techMap/techMap.js
@@ -11,6 +11,7 @@ window.techMapItemClick = function techMapItemClick (event) {
     let descArea = $(techMapRoot).children("div[tag='" + descriptionTag + "']")
     descArea.css('display', 'flex')
 
+    $(event.target).parent().css('flex-grow', '1.05')
     $(event.target).css('border', '2px solid white')
     $(event.target).css('padding', EXPAND_STATE)
     $(event.target).attr('expanded', 'true')
@@ -21,6 +22,8 @@ window.techMapItemClick = function techMapItemClick (event) {
 }
 
 function clearAllEffects (techMapRoot) {
+  // reset flex
+  $(techMapRoot).children('div.row').children('div.col').css('flex-grow', '1.0')
   // hide all description areas
   $(techMapRoot).children('div.description-area').css('display', 'none')
 
@@ -29,4 +32,24 @@ function clearAllEffects (techMapRoot) {
   // remove expand
   $(techMapRoot).children('div.row').children().children().css('padding', '')
   $(techMapRoot).children('div.row').children().children().attr('expanded', 'false')
+}
+
+// return true if a column has an expanded element
+function hasExpanded (col) {
+  let expandedItems = $(col).children('button[expanded=\'true\']')
+  if (expandedItems.length != 0) {
+    return true
+  }
+  return false
+}
+
+// do manual onHover handling as CSS :hover will not work if you set properties from JS.
+window.techMapHoverIn = function hoverIn (event) {
+  $(event.target).css('flex-grow', '1.05')
+}
+
+window.techMapHoverOut = function hoverOut (event) {
+  if (!hasExpanded(event.target)) {
+    $(event.target).css('flex-grow', '1.0')
+  }
 }
